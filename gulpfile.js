13ac2f9c9d4ec9902ed891fc6c7984caa8abe9f9
Bharath <bharathvaj1995@gmail.com>
2017-09-23 14:41:12 +0530

    Fix version and added desc

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,18 @@ const uglify = require('gulp-uglify');
 const pkg = require('./package.json');
 const clean = require('gulp-clean');
 
+const bannerString =
+	'/*\n' +
+	' * <%= pkg.name %> <%= pkg.version %>\n' +
+	' * <%= pkg.description %>\n' +
+	' * <%= pkg.homepage %>\n' +
+	' *\n' +
+	' * Copyright ' +
+	new Date().getFullYear() +
+	', <%= pkg.author %>\n' +
+	' * Released under the <%= pkg.license %> license.\n' +
+	'*/\n\n';
+
 gulp.task('clean', () => {
 	return gulp.src('dist/*.js', { read: false }).pipe(clean());
 });
